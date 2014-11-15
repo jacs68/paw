0ad1975e47f63620f68cd28596fceeedfefdceaa
Sistemas <Sistemas@10.1.31.2>
2014-11-14 21:22:01 -0500

    Usuario creado

---
+++ src/java/modelo/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Sistemas
+ */
+public class Usuario {
+    private String user;
+    private String password;
+    private boolean activo;
+
+    public Usuario() {
+    }
+
+    public Usuario(String user, String password) {
+        this.user = user;
+        this.password = password;
+        activo=true;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
+    
+}
